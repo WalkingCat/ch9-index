68a91988fa88918c96762cca5c0a4f6f6f1094b9
WalkingCat <walking.cat@hotmail.com>
2021-12-04 15:42:40 +0800

    add src

---
+++ src/build.cs
@@ -0,0 +1,95 @@
+﻿using System.Xml;
+
+var index_files = new Dictionary<string, StreamWriter>();
+foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", args.FirstOrDefault(""))) {
+    var filename = Path.GetFileNameWithoutExtension(path);
+    var name_start = filename.IndexOf("_", 8);
+    var cat = filename.Substring(8, name_start - 8);
+    var name = filename.Substring(name_start + 1);
+    var title = name.Replace('+', ' ');
+    Console.WriteLine($"{cat} {name}");
+
+    var xml = new XmlDocument();
+    xml.Load(path);
+
+    using var content = new StreamWriter($"..\\docs\\{cat}_{name}.html", false, System.Text.Encoding.UTF8);
+    var index_name = $"..\\docs\\{cat}.html";
+    if (!index_files.ContainsKey(index_name)) {
+        index_files[index_name] = new StreamWriter(index_name, false, System.Text.Encoding.UTF8);
+    }
+    var index = index_files[index_name];
+
+    var first = true;
+    var urls = xml.GetElementsByTagName("url");
+    foreach (XmlElement elem in urls) {
+        var video = elem.GetElementsByTagName("video:video").Item(0) as XmlElement;
+        var loc = elem.GetElementsByTagName("loc").Item(0)?.InnerText;
+        if (first) {
+            if (index.BaseStream.Position == 0) {
+                index.WriteLine(
+                    "<head><link rel='stylesheet' href='styles.css'></head>"
+                );
+            }
+            index.WriteLine(
+                "<nobr>" +
+                $"<a href='http://web.archive.org/web/2020/{loc}' target='_blank'><img src='logo_archive-sm.png' width=24 height=24></a> " +
+                $"<a href='{cat}_{name}.html' target='content'>{title}</a> ({((cat == "Posts") ? urls.Count : (urls.Count - 1))})" +
+                "</nobr><br/>"
+            );
+            index.Flush();
+            first = false;
+
+            content.WriteLine(
+                "<head><link rel='stylesheet' href='styles.css'></head>"
+            );
+            if (cat == "Posts") {
+                content.WriteLine(
+                    $"<nobr><h2>{cat} - {title}</h2></nobr><br/>"
+                );
+            } else {
+                content.WriteLine(
+                    "<nobr><h2>" +
+                    $"<a href='http://web.archive.org/web/2020/{loc}' target='_blank'><img src='logo_archive-sm.png' width=24 height=24></a> " +
+                    $"{cat} - {title}" +
+                    "</h2></nobr><br/>"
+                );
+                continue;
+            }
+        }
+        {
+            var v_title = video?.GetElementsByTagName("video:title").Item(0)?.InnerText;
+            var v_desc = video?.GetElementsByTagName("video:description").Item(0)?.InnerText;
+            var v_loc = video?.GetElementsByTagName("video:content_loc").Item(0)?.InnerText;
+            var v_date = video?.GetElementsByTagName("video:publication_date").Item(0)?.InnerText;
+            var v_thumb = video?.GetElementsByTagName("video:thumbnail_loc").Item(0)?.InnerText;
+            var v_time = long.Parse(video?.GetElementsByTagName("video:duration").Item(0)?.InnerText ?? "-1");
+
+            v_thumb = v_thumb?.Replace("http://video.ch9.ms/", "https://sec.ch9.ms/");
+            v_loc = v_loc?.Replace("http://video.ch9.ms/", "https://sec.ch9.ms/");
+
+            if (string.IsNullOrWhiteSpace(v_title)) {
+                if (!string.IsNullOrWhiteSpace(loc)) {
+                    v_title = $"[{new System.Uri(loc).Segments.Last()}]";
+                } else {
+                    v_title = "[Title Missing]";
+                }
+            }
+            content.WriteLine(
+                "<nobr>" +
+                $"<a href='http://web.archive.org/web/2020/{loc}' target='_blank'><img src='logo_archive-sm.png' width=24 height=24></a> " +
+                ((v_loc is object) ? $"<a href='{v_loc}' target='_blank'>{System.Web.HttpUtility.HtmlDecode(v_title)}</a> " : System.Web.HttpUtility.HtmlDecode(v_title)) +
+                ((v_time >= 0) ? $"[{v_time / 3600}:{(v_time % 3600) / 60:D2}:{v_time % 60:D2}] " : "") +
+                ((v_date is object) ? DateTime.Parse(v_date).ToString("[yyyy/MM/dd]") : "") +
+                "</nobr><br/>" +
+                ((v_thumb is object) ? $"<img class='vthumb' src='{v_thumb}'/>" : "") +
+                $"<div class='vdesc'>{(System.Web.HttpUtility.HtmlDecode(v_desc))}</div>" +
+                "<br clear='left'/><br/>"
+            );
+        }
+    }
+}
+foreach (var (_, index) in index_files) {
+    index.Flush();
+    index.Close();
+    index.Dispose();
+}
