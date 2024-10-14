bacc1b26aae1eded17fe3202e3e991e72561ca04
WalkingCat <walking.cat@hotmail.com>
2024-10-14 21:23:28 +0800

    fix Blogs/DavidAiken

--- src/build.cs
+++ src/build.cs
@@ -8,8 +8,7 @@ var has_filter = !string.IsNullOrEmpty(filter);
 var indexes = new Dictionary<string, Tuple<StringWriter, StringWriter>>();
 var http = new HttpClient();
 
-foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
-{
+foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter)) {
     var filename = Path.GetFileNameWithoutExtension(path);
     var name_start = filename.IndexOf("_", 8);
     var cat = filename.Substring(8, name_start - 8);
@@ -20,10 +19,8 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
     xml.Load(path);
 
     var index_name = $"{out_dir}{cat}.html";
-    if (!has_filter)
-    {
-        if (!indexes.ContainsKey(index_name))
-        {
+    if (!has_filter) {
+        if (!indexes.ContainsKey(index_name)) {
             indexes[index_name] = new(new StringWriter(), new StringWriter());
         }
     }
@@ -36,8 +33,7 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
     var arc_date = "2020";
     var featured = false;
 
-    foreach (XmlElement elem in urls)
-    {
+    foreach (XmlElement elem in urls) {
         var video = elem.GetElementsByTagName("video:video").Item(0) as XmlElement;
         var loc = elem.GetElementsByTagName("loc").Item(0)?.InnerText;
         var loc_name = Path.GetFileName(loc);
@@ -46,11 +42,9 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
         var desc = elem.GetElementsByTagName("description").Item(0)?.InnerText;
         var thumb = elem.GetElementsByTagName("thumbnail_loc").Item(0)?.InnerText;
 
-        if (first)
-        {
+        if (first) {
             featured = elem.GetElementsByTagName("featured").Item(0)?.InnerText == "true";
-            if (featured && !string.IsNullOrEmpty(thumb) && !thumb.StartsWith("."))
-            {
+            if (featured && !string.IsNullOrEmpty(thumb) && !thumb.StartsWith(".")) {
                 var local_thumb = $"./thumbnails/{cat}_{name}{Path.GetExtension(thumb)}";
                 if (has_filter) DownloadFile(thumb, local_thumb);
                 thumb = local_thumb;
@@ -61,8 +55,7 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
             var link = (cat == "Posts") ? $"https://channel9.msdn.com/Niners/{name}/Posts" : loc;
 
             first = false;
-            if (index_writers is object)
-            {
+            if (index_writers is object) {
                 var index = featured ? index_writers.Item1 : index_writers.Item2;
                 index.WriteLine(
                      $"<nobr id='{name}' class='title-container{(featured ? " featured" : "")}'>" +
@@ -80,8 +73,7 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
                 $"<a href='https://web.archive.org/web/{arc_date}/{link}' target='_blank'>{cat} - {title}</a>" +
                 "</span></h2></nobr>"
             );
-            if (!(string.IsNullOrEmpty(thumb) && string.IsNullOrEmpty(desc)))
-            {
+            if (!(string.IsNullOrEmpty(thumb) && string.IsNullOrEmpty(desc))) {
                 content.Write(
                     (string.IsNullOrEmpty(thumb) ? "" : $"<img class='thumb' src='{thumb}'/>") +
                     $"<div class='desc'>{(System.Web.HttpUtility.HtmlDecode(desc))}</div>" +
@@ -89,8 +81,7 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
                 );
             }
             content.WriteLine("<br/>");
-            if (cat != "Posts")
-            {
+            if (cat != "Posts") {
                 continue;
             }
         }
@@ -107,14 +98,11 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
 
             var v_res = new List<Resource>();
             var v_locs = video?.GetElementsByTagName("video:content_loc");
-            if (v_locs is object)
-            {
-                for (var i = 0; i < v_locs.Count; ++i)
-                {
+            if (v_locs is object) {
+                for (var i = 0; i < v_locs.Count; ++i) {
                     var item = v_locs.Item(i)!;
                     var v_loc = item.InnerText;
-                    if (!string.IsNullOrEmpty(v_loc))
-                    {
+                    if (!string.IsNullOrEmpty(v_loc)) {
                         bool v_fixed = (item.Attributes?["fixed"]?.InnerText == "true");
                         if (!v_fixed) {
                             v_loc = v_loc.Replace("http://video.ch9.ms/", "https://sec.ch9.ms/");
@@ -125,26 +113,19 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
                 }
             }
 
-			if (!string.IsNullOrEmpty(v_thumb) && !v_thumb.StartsWith("."))
-			{
-				if (featured)
-				{
-					var local_thumb = $"./thumbnails/{cat}_{name}/" + Path.ChangeExtension(loc_name, Path.GetExtension(v_thumb));
-					if (has_filter) DownloadFile(v_thumb, local_thumb);
-					v_thumb = local_thumb;
-				} else {
-					v_thumb = "https://web.archive.org/web/2020if_/" + v_thumb;
-				}
-			}
-			
-            if (string.IsNullOrWhiteSpace(v_title))
-            {
-                if (!string.IsNullOrWhiteSpace(loc))
-                {
-                    v_title = $"[{new System.Uri(loc).Segments.Last()}]";
+            if (!string.IsNullOrEmpty(v_thumb) && !v_thumb.StartsWith(".")) {
+                v_thumb = "https://web.archive.org/web/2020if_/" + v_thumb;
+                if (featured) {
+                    var local_thumb = $"./thumbnails/{cat}_{name}/" + Path.ChangeExtension(loc_name, Path.GetExtension(v_thumb));
+                    if (has_filter) DownloadFile(v_thumb, local_thumb);
+                    v_thumb = local_thumb;
                 }
-                else
-                {
+            }
+
+            if (string.IsNullOrWhiteSpace(v_title)) {
+                if (!string.IsNullOrWhiteSpace(loc)) {
+                    v_title = $"[{new System.Uri(loc).Segments.Last()}]";
+                } else {
                     v_title = "[Title Missing]";
                 }
             }
@@ -158,20 +139,17 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
                 ((v_thumb is object) ? $"<img class='vthumb' src='{v_thumb}'/>" : "")
             );
             content.Write("<nobr class='vinfo-container'><span class='vinfo'>");
-            foreach (var res in v_res)
-            {
+            foreach (var res in v_res) {
                 var ext = Path.GetExtension(res.Location).TrimStart('.').ToUpper();
-                if (string.IsNullOrEmpty(ext))
-                {
+                if (string.IsNullOrEmpty(ext)) {
                     ext = "LINK";
                 }
                 var res_label = (res.Label is object) ? $"({res.Label.ToUpper()})" : "";
                 var res_arc_date = res.ArchiveDate;
-				var res_loc = res.Location;
-				if (cat != "Extras")
-				{
-					res_loc = $"https://web.archive.org/web/{res_arc_date ?? arc_date}if_/" + res_loc;
-				}
+                var res_loc = res.Location;
+                if (cat != "Extras") {
+                    res_loc = $"https://web.archive.org/web/{res_arc_date ?? arc_date}if_/" + res_loc;
+                }
                 content.Write($"<a href='{res_loc}' target='_blank'>[{ext}{res_label}]</a> ");
             }
             content.Write(
@@ -186,8 +164,7 @@ foreach (var path in Directory.EnumerateFiles(@"..\sitemaps\", filter))
     }
 }
 
-foreach (var (name, index) in indexes)
-{
+foreach (var (name, index) in indexes) {
     var featured = index.Item1.ToString();
     var archive = index.Item2.ToString();
 
@@ -196,13 +173,11 @@ foreach (var (name, index) in indexes)
     index_file.WriteLine(
         "<head><link rel='stylesheet' href='styles.css'></head><body class='index'>"
     );
-    if (featured.Length > 0)
-    {
+    if (featured.Length > 0) {
         index_file.WriteLine("<hr class='featured-begin'/>");
     }
     index_file.Write(featured);
-    if ((featured.Length > 0) && (archive.Length > 0))
-    {
+    if ((featured.Length > 0) && (archive.Length > 0)) {
         index_file.WriteLine("<hr class='featured'/>");
     }
     index_file.Write(archive);
@@ -214,44 +189,32 @@ foreach (var (name, index) in indexes)
 void DownloadFile(string uri, string file)
 {
     var path = Path.Combine(out_dir, file);
-    if (!File.Exists(path))
-    {
+    if (!File.Exists(path)) {
         bool done = false;
-        do
-        {
-            try
-            {
+        do {
+            try {
                 Console.Write($"  Downloading {file}");
                 Directory.CreateDirectory(Path.GetDirectoryName(path)!);
                 var data = http.GetByteArrayAsync(uri).Result;
-                if (data.Length > 0)
-                {
+                if (data.Length > 0) {
                     using var fs = File.OpenWrite(path);
                     fs.Write(data);
                     fs.Flush();
                     fs.Close();
-                }
-                else
-                {
+                } else {
                     Console.Write(" [EMPTY]");
                 }
                 done = true;
-            }
-            catch (AggregateException ex)
-            {
-                if (ex.InnerException is HttpRequestException hrex)
-                {
+            } catch (AggregateException ex) {
+                if (ex.InnerException is HttpRequestException hrex) {
                     Console.Write($" [{hrex.StatusCode}]");
                     if ((hrex.StatusCode == System.Net.HttpStatusCode.NotFound)
-                        || (hrex.StatusCode == System.Net.HttpStatusCode.GatewayTimeout))
-                    {
+                        || (hrex.StatusCode == System.Net.HttpStatusCode.GatewayTimeout)
+                        || (hrex.StatusCode == System.Net.HttpStatusCode.Conflict)) {
                         done = true;
                     }
-                }
-                else throw;
-            }
-            catch
-            {
+                } else throw;
+            } catch {
                 Console.Write(" [ERROR]");
                 done = true;
             }
